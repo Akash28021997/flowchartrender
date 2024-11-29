22c231934a11a4f4d8a121c6a3c2b1c097936f79
IT Developer <akash.pawar@rkdewan.com>
2024-11-29 09:47:39 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "https://flow-chart-render.onrender.com"}})
 
 @app.route('/', methods=['POST'])
 def process_data():
